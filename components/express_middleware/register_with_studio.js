c40acb00091b389a170005333dc7433a47d1d6af
erwin foxtree <foxtree@gmail.com>
2017-04-12 14:47:20 -0400

    eslint --fix

--- components/express_middleware/register_with_studio.js
+++ components/express_middleware/register_with_studio.js
@@ -1,55 +1,48 @@
-var request = require('request');
-var debug = require('debug')('botkit:register_with_studio');
+const request = require('request');
+const debug = require('debug')('botkit:register_with_studio');
 module.exports = function(webserver, controller) {
+  let registered_this_session = false;
 
-    var registered_this_session = false;
-
-    if (webserver && controller.config.studio_token) {
-        webserver.use(function(req, res, next) {
-            if (!registered_this_session) {
+  if (webserver && controller.config.studio_token) {
+    webserver.use((req, res, next) => {
+      if (!registered_this_session) {
                 // get URL from the request
-                var host = req.get('host');
+        const host = req.get('host');
 
                 // information about this instance of Botkit
                 // send to Botkit Studio in order to display in the hosting tab
-                var instance = {
-                    url: host,
-                    version: controller.version(),
-                    ts: new Date(),
-                }
+        const instance = {
+          url: host,
+          version: controller.version(),
+          ts: new Date()
+        };
 
-                request({
-                    method: 'post',
-                    uri: (controller.config.studio_command_uri || 'https://studio.botkit.ai') + '/api/v1/bots/phonehome?access_token=' + controller.config.studio_token,
-                    form: instance,
-                },function(err, res, body) {
+        request({
+          method: 'post',
+          uri: `${controller.config.studio_command_uri || 'https://studio.botkit.ai'}/api/v1/bots/phonehome?access_token=${controller.config.studio_token}`,
+          form: instance
+        }, (err, res, body) => {
+          registered_this_session = true;
 
-                    registered_this_session = true;
-
-                    if (err) {
-                        debug('Error registering instance with Botkit Studio', err);
-                    } else {
-
-                        var json =null;
-                        try {
-                            json = JSON.parse(body);
-                        } catch(err) {
-                            debug('Error registering instance with Botkit Studio', err);
-                        }
-
-                        if (json) {
-                            if (json.error) {
-                                debug('Error registering instance with Botkit Studio', json.error);
-                            }
-                        }
-                    }
-
-                });
-
+          if (err) {
+            debug('Error registering instance with Botkit Studio', err);
+          } else {
+            let json = null;
+            try {
+              json = JSON.parse(body);
+            } catch (err) {
+              debug('Error registering instance with Botkit Studio', err);
             }
-            next();
+
+            if (json) {
+              if (json.error) {
+                debug('Error registering instance with Botkit Studio', json.error);
+              }
+            }
+          }
         });
-
-    }
-
-}
+      }
+      next();
+    });
+  }
+};
