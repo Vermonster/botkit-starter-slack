ac7ad4bac2424a8e0f841afa7ded9f9f643dfdb6
erwin foxtree <foxtree@gmail.com>
2017-04-13 11:14:06 -0400

    eslint: fixup components/express_webserver.js

--- components/express_webserver.js
+++ components/express_webserver.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const querystring = require('querystring');
+// const querystring = require('querystring');
 const debug = require('debug')('botkit:webserver');
+
+const DEFAULT_PORT = 3000;
+const PORT = process.env.PORT || DEFAULT_PORT;
 
 module.exports = function(controller) {
   const webserver = express();
@@ -9,24 +14,26 @@ module.exports = function(controller) {
   webserver.use(bodyParser.urlencoded({ extended: true }));
 
   // import express middlewares that are present in /components/express_middleware
-  var normalizedPath = require('path').join(__dirname, 'express_middleware');
-  require('fs').readdirSync(normalizedPath).forEach((file) => {
+  const middlewarePath = path.join(__dirname, 'express_middleware');
+  fs.readdirSync(middlewarePath).forEach((file) => {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
     require(`./express_middleware/${file}`)(webserver, controller);
   });
 
   webserver.use(express.static('public'));
 
-
-  webserver.listen(process.env.PORT || 3000, null, () => {
-    debug(`Express webserver configured and listening at http://localhost:${process.env.PORT}` || 3000);
+  webserver.listen(PORT, null, () => {
+    debug(`Express webserver configured and listening at http://localhost:${PORT}`);
   });
 
   // import all the pre-defined routes that are present in /components/routes
-  var normalizedPath = require('path').join(__dirname, 'routes');
-  require('fs').readdirSync(normalizedPath).forEach((file) => {
+  const routesPath = path.join(__dirname, 'routes');
+  fs.readdirSync(routesPath).forEach((file) => {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
     require(`./routes/${file}`)(webserver, controller);
   });
 
+  // eslint-disable-next-line no-param-reassign
   controller.webserver = webserver;
 
   return webserver;
