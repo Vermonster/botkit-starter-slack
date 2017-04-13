1e293f56257bcbd3405f200f2768600d74f24458
erwin foxtree <foxtree@gmail.com>
2017-04-13 11:26:30 -0400

    eslint: fixup bot.js

--- bot.js
+++ bot.js
@@ -52,56 +52,73 @@ require('./build-support/check-node-version');
  -> http://howdy.ai/botkit
 
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+const fs = require('fs');
+const path = require('path');
 const env = require('node-env-file');
+
 env(`${__dirname}/.env`);
 
+const pluginDashbot = require('./components/plugin_dashbot.js');
 
-if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
-  console.log('Error: Specify clientId clientSecret and PORT in environment');
-  usage_tip();
-  process.exit(1);
+const usageTip = function() {
+  console.log('~~~~~~~~~~');
+  console.log('Botkit Starter Kit');
+  console.log('Execute your bot application like this:');
+  console.log('clientId=<SLACK CLIENT ID> clientSecret=<CLIENT SECRET> PORT=3000 node bot.js');
+  console.log('Get Slack app credentials here: https://api.slack.com/apps');
+  console.log('Get a Botkit Studio token here: https://studio.botkit.ai/');
+  console.log('~~~~~~~~~~');
+};
+
+const { clientId, clientSecret, PORT, studio_token, studio_command_uri } = process.env;
+
+if (!clientId || !clientSecret || !PORT) {
+  usageTip();
+  throw new Error('Specify clientId clientSecret and PORT in environment');
 }
 
 const Botkit = require('botkit');
+const bindWebserverToController = require('./components/express_webserver.js');
 const debug = require('debug')('botkit:main');
 
 // Create the Botkit controller, which controls all instances of the bot.
 const controller = Botkit.slackbot({
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
+  clientId,
+  clientSecret,
   // debug: true,
   scopes: ['bot'],
-  studio_token: process.env.studio_token,
-  studio_command_uri: process.env.studio_command_uri,
+  studio_token,
+  studio_command_uri,
   json_file_store: `${__dirname}/.db/` // store user data in a simple JSON format
 });
 
 controller.startTicking();
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
-const webserver = require(`${__dirname}/components/express_webserver.js`)(controller);
+bindWebserverToController(controller);
 
 // Set up a simple storage backend for keeping a record of customers
 // who sign up for the app via the oauth
-require(`${__dirname}/components/user_registration.js`)(controller);
+require('./components/user_registration.js')(controller);
 
 // Send an onboarding message when a new team joins
-require(`${__dirname}/components/onboarding.js`)(controller);
+require('./components/onboarding.js')(controller);
 
 // no longer necessary since slack now supports the always on event bots
-// // Set up a system to manage connections to Slack's RTM api
-// // This will eventually be removed when Slack fixes support for bot presence
-// var rtm_manager = require(__dirname + '/components/rtm_manager.js')(controller);
+// Set up a system to manage connections to Slack's RTM api
+// This will eventually be removed when Slack fixes support for bot presence
+// const rtmManager = require('./components/rtm_manager.js')(controller);
 //
 // // Reconnect all pre-registered bots
-// rtm_manager.reconnect();
+// rtmManager.reconnect();
 
 // Enable Dashbot.io plugin
-require(`${__dirname}/components/plugin_dashbot.js`)(controller);
+pluginDashbot(controller);
 
 
-const normalizedPath = require('path').join(__dirname, 'skills');
-require('fs').readdirSync(normalizedPath).forEach((file) => {
+const normalizedPath = path.join(__dirname, 'skills');
+fs.readdirSync(normalizedPath).forEach((file) => {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
   require(`./skills/${file}`)(controller);
 });
 
@@ -135,15 +152,4 @@ if (process.env.studio_token) {
   console.log('~~~~~~~~~~');
   console.log('NOTE: Botkit Studio functionality has not been enabled');
   console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
-
-
-function usage_tip() {
-  console.log('~~~~~~~~~~');
-  console.log('Botkit Starter Kit');
-  console.log('Execute your bot application like this:');
-  console.log('clientId=<MY SLACK CLIENT ID> clientSecret=<MY CLIENT SECRET> PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-  console.log('Get Slack app credentials here: https://api.slack.com/apps');
-  console.log('Get a Botkit Studio token here: https://studio.botkit.ai/');
-  console.log('~~~~~~~~~~');
 }
