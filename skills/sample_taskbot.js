333be348b60084197095e8e6c5a9d62e882e9f9b
erwin foxtree <foxtree@gmail.com>
2017-04-14 10:38:51 -0400

    eslint: fixup skills/sample_taskbot.js

--- skills/sample_taskbot.js
+++ skills/sample_taskbot.js
@@ -18,7 +18,19 @@
 
  */
 
+const createNewUser = function({ user }) {
+  return {
+    id: user,
+    tasks: []
+  };
+};
+
 module.exports = function(controller) {
+  // simple function to generate the text of the task list so that
+  // it can be used in various places
+  const formatTasks = (text, taskItem, index) => `${text}> \`${index + 1}\`) ${taskItem}\n`;
+  const generateTaskList = user => user.tasks.reduce(formatTasks, '');
+
   // listen for someone saying 'tasks' to the bot
   // reply with a list of current tasks loaded from the storage system
   // based on this user's id
@@ -26,13 +38,11 @@ module.exports = function(controller) {
     // load user from storage...
     controller.storage.users.get(message.user, (err, user) => {
       // user object can contain arbitary keys. we will store tasks in .tasks
-      if (!user || !user.tasks || user.tasks.length == 0) {
+      if (!user || !user.tasks || user.tasks.length === 0) {
         bot.reply(message, 'There are no tasks on your list. Say `add _task_` to add something.');
       } else {
-        const text = `Here are your current tasks: \n${
-          generateTaskList(user)
-          }Reply with \`done _number_\` to mark a task completed.`;
-
+        let text = `Here are your current tasks: \n${generateTaskList(user)}`;
+        text += 'Reply with `done _number_` to mark a task completed.';
         bot.reply(message, text);
       }
     });
@@ -42,18 +52,14 @@ module.exports = function(controller) {
   // store the new list in the storage system
   controller.hears(['add (.*)'], 'direct_message,direct_mention,mention', (bot, message) => {
     const newtask = message.match[1];
-    controller.storage.users.get(message.user, (err, user) => {
-      if (!user) {
-        user = {};
-        user.id = message.user;
-        user.tasks = [];
-      }
+    controller.storage.users.get(message.user, (getUserError, user) => {
+      const currentUser = user || createNewUser(message);
 
-      user.tasks.push(newtask);
+      currentUser.tasks.push(newtask);
 
-      controller.storage.users.save(user, (err, saved) => {
-        if (err) {
-          bot.reply(message, `I experienced an error adding your task: ${err}`);
+      controller.storage.users.save(currentUser, (saveTaskError, _saved) => {
+        if (saveTaskError) {
+          bot.reply(message, `I experienced an error adding your task: ${saveTaskError}`);
         } else {
           bot.api.reactions.add({
             name: 'thumbsup',
@@ -67,31 +73,26 @@ module.exports = function(controller) {
 
   // listen for a user saying "done <number>" and mark that item as done.
   controller.hears(['done (.*)'], 'direct_message', (bot, message) => {
-    let number = message.match[1];
-
-    if (isNaN(number)) {
+    const taskNumber = message.match[1];
+    if (isNaN(taskNumber)) {
       bot.reply(message, 'Please specify a number.');
     } else {
-      // adjust for 0-based array index
-      number = parseInt(number) - 1;
+      const taskIndex = parseInt(taskNumber, 10) - 1;
+      controller.storage.users.get(message.user, (getUserError, user) => {
+        const currentUser = user || createNewUser(message);
 
-      controller.storage.users.get(message.user, (err, user) => {
-        if (!user) {
-          user = {};
-          user.id = message.user;
-          user.tasks = [];
-        }
-
-        if (number < 0 || number >= user.tasks.length) {
-          bot.reply(message, `Sorry, your input is out of range. Right now there are ${user.tasks.length} items on your list.`);
+        if (taskIndex < 0 || taskIndex >= currentUser.tasks.length) {
+          let replyMessage = 'Sorry, your input is out of range.\n';
+          replyMessage += `Right now there are ${currentUser.tasks.length} items on your list.`;
+          bot.reply(message, replyMessage);
         } else {
-          const item = user.tasks.splice(number, 1);
+          const taskItem = currentUser.tasks.splice(taskIndex, 1);
 
           // reply with a strikethrough message...
-          bot.reply(message, `~${item}~`);
+          bot.reply(message, `~${taskItem}~`);
 
-          if (user.tasks.length > 0) {
-            bot.reply(message, `Here are our remaining tasks:\n${generateTaskList(user)}`);
+          if (currentUser.tasks.length > 0) {
+            bot.reply(message, `Here are our remaining tasks:\n${generateTaskList(currentUser)}`);
           } else {
             bot.reply(message, 'Your list is now empty!');
           }
@@ -99,16 +100,4 @@ module.exports = function(controller) {
       });
     }
   });
-
-  // simple function to generate the text of the task list so that
-  // it can be used in various places
-  function generateTaskList(user) {
-    let text = '';
-
-    for (let t = 0; t < user.tasks.length; t++) {
-      text = `${text}> \`${t + 1}\`) ${user.tasks[t]}\n`;
-    }
-
-    return text;
-  }
 };
