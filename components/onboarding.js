2cf7a4d577276d2d082c277d439addc4475ce4d0
erwin foxtree <foxtree@gmail.com>
2017-04-14 11:54:27 -0400

    eslint: fixup components/onboarding.js

--- components/onboarding.js
+++ components/onboarding.js
@@ -5,19 +5,19 @@ module.exports = function(controller) {
     debug('Starting an onboarding experience!');
 
     if (controller.config.studio_token) {
-      bot.api.im.open({ user: bot.config.createdBy }, (err, direct_message) => {
-        if (err) {
-          debug('Error sending onboarding message:', err);
+      bot.api.im.open({ user: bot.config.createdBy }, (onboardError, directMessage) => {
+        if (onboardError) {
+          debug('Error sending onboarding message:', onboardError);
         } else {
-          controller.studio.run(bot, 'onboarding', bot.config.createdBy, direct_message.channel.id).catch((err) => {
+          controller.studio.run(bot, 'onboarding', bot.config.createdBy, directMessage.channel.id).catch((err) => {
             debug('Error: encountered an error loading onboarding script from Botkit Studio:', err);
           });
         }
       });
     } else {
-      bot.startPrivateConversation({ user: bot.config.createdBy }, (err, convo) => {
-        if (err) {
-          console.log(err);
+      bot.startPrivateConversation({ user: bot.config.createdBy }, (startConvoError, convo) => {
+        if (startConvoError) {
+          console.log(startConvoError);
         } else {
           convo.say('I am a bot that has just joined your team');
           convo.say('You must now /invite me to a channel so that I can be of use!');
