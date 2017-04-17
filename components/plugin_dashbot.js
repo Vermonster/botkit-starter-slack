c81d0ce1d3fec5d45775e7cbfde76ce40cb98d46
erwin foxtree <foxtree@gmail.com>
2017-04-17 13:51:20 -0400

    eslint: fixup components/plugin_dashbot.js

--- components/plugin_dashbot.js
+++ components/plugin_dashbot.js
@@ -1,12 +1,18 @@
+const Dashbot = require('dashbot');
+
 module.exports = function(controller) {
   // Dashbot is a turnkey analytics platform for bots.
   // Sign up for a free key here: https://www.dashbot.io/ to see your bot analytics in real time.
-  if (process.env.DASHBOT_API_KEY) {
-    const dashbot = require('dashbot')(process.env.DASHBOT_API_KEY).slack;
+  const dashbotKey = process.env.DASHBOT_API_KEY;
+  if (dashbotKey) {
+    const dashbot = Dashbot(dashbotKey).slack;
     controller.middleware.receive.use(dashbot.receive);
     controller.middleware.send.use(dashbot.send);
-    controller.log.info('Thanks for using Dashbot. Visit https://www.dashbot.io/ to see your bot analytics in real time.');
+    const hasKeyMsg = 'Thanks for using Dashbot. Visit https://www.dashbot.io/ to see your bot analytics in real time.';
+    controller.log.info(hasKeyMsg);
   } else {
-    controller.log.info('No DASHBOT_API_KEY specified. For free turnkey analytics for your bot, go to https://www.dashbot.io/ to get your key.');
+    let noKeyMsg = 'No DASHBOT_API_KEY specified. \n';
+    noKeyMsg += 'For free turnkey analytics for your bot, go to https://www.dashbot.io/ to get your key.';
+    controller.log.info(noKeyMsg);
   }
 };
