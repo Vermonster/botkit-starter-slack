9924431e7b3ef844276a0ee13d260056445010bc
erwin foxtree <foxtree@gmail.com>
2017-04-17 13:15:11 -0400

    eslint: fixup components/routes/incoming_webhooks.js

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -1,12 +1,14 @@
 const debug = require('debug')('botkit:incoming_webhooks');
 
+const HTTP_STATUS_OK = 200;
+
 module.exports = function(webserver, controller) {
   debug('Configured /slack/receive url');
   webserver.post('/slack/receive', (req, res) => {
     // NOTE: we should enforce the token check here
 
     // respond to Slack that the webhook has been received.
-    res.status(200);
+    res.status(HTTP_STATUS_OK);
 
     // Now, pass the webhook into be processed
     controller.handleWebhookPayload(req, res);
