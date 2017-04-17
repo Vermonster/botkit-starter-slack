21599e64124dc850892e9b123dde5f39a9b82ac5
erwin foxtree <foxtree@gmail.com>
2017-04-17 13:38:56 -0400

    eslint: fixup components/rtm_manager.js

--- components/rtm_manager.js
+++ components/rtm_manager.js
@@ -1,7 +1,47 @@
 const debug = require('debug')('botkit:rtm_manager');
 
 module.exports = function(controller) {
-  const managed_bots = {};
+  const managedBots = {};
+
+  // The manager object exposes some useful tools for managing the RTM
+  const manager = {
+    start(bot) {
+      if (managedBots[bot.config.token]) {
+        debug('Start RTM: already online');
+      } else {
+        bot.startRTM((err, rtmBot) => {
+          if (err) {
+            debug('Error starting RTM:', err);
+          } else {
+            managedBots[rtmBot.config.token] = rtmBot.rtm;
+            debug('Start RTM: Success');
+          }
+        });
+      }
+    },
+    stop(bot) {
+      if (managedBots[bot.config.token]) {
+        if (managedBots[bot.config.token].rtm) {
+          debug('Stop RTM: Stopping bot');
+          managedBots[bot.config.token].closeRTM();
+        }
+      }
+    },
+    remove(bot) {
+      debug('Removing bot from manager');
+      delete managedBots[bot.config.token];
+    },
+    reconnect() {
+      debug('Reconnecting all existing bots...');
+      controller.storage.teams.all((err, list) => {
+        if (err) {
+          throw new Error('Error: Could not load existing bots:', err);
+        } else {
+          list.forEach(team => manager.start(controller.spawn(team.bot)));
+        }
+      });
+    }
+  };
 
   // Capture the rtm:start event and actually start the RTM...
   controller.on('rtm:start', (config) => {
@@ -13,49 +53,6 @@ module.exports = function(controller) {
   controller.on('rtm_close', (bot) => {
     manager.remove(bot);
   });
-
-  // The manager object exposes some useful tools for managing the RTM
-  var manager = {
-    start(bot) {
-      if (managed_bots[bot.config.token]) {
-        debug('Start RTM: already online');
-      } else {
-        bot.startRTM((err, bot) => {
-          if (err) {
-            debug('Error starting RTM:', err);
-          } else {
-            managed_bots[bot.config.token] = bot.rtm;
-            debug('Start RTM: Success');
-          }
-        });
-      }
-    },
-    stop(bot) {
-      if (managed_bots[bot.config.token]) {
-        if (managed_bots[bot.config.token].rtm) {
-          debug('Stop RTM: Stopping bot');
-          managed_bots[bot.config.token].closeRTM();
-        }
-      }
-    },
-    remove(bot) {
-      debug('Removing bot from manager');
-      delete managed_bots[bot.config.token];
-    },
-    reconnect() {
-      debug('Reconnecting all existing bots...');
-      controller.storage.teams.all((err, list) => {
-        if (err) {
-          throw new Error('Error: Could not load existing bots:', err);
-        } else {
-          for (let l = 0; l < list.length; l++) {
-            manager.start(controller.spawn(list[l].bot));
-          }
-        }
-      });
-    }
-  };
-
 
   return manager;
 };
